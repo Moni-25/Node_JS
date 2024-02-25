15b96db999bfa8d00a6f8f49c5c9cf40cb420393
Moni-25 <mariyacelestina@gmail.com>
2024-02-25 19:37:07 +0530

    Install CORS

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const APP_SERVER = express();
+const cors = require("cors");
+
+
+APP_SERVER.use(cors());
 
 // Inject API Controller
 APP_SERVER.use("/api/mentor", require("./controller/mentorsController"));
