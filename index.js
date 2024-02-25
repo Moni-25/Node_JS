b7a2373187f35b5dad0cde3a55d51a8acde71093
Moni-25 <mariyacelestina@gmail.com>
2024-02-25 19:47:16 +0530

    Add Hostname

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const HTTP_SERVER = express();
 const port = 5000;
+const hostname = "0.0.0.0";
 const { connectDatabase } = require("./dbConfigure");
 
 connectDatabase();
 HTTP_SERVER.use(bodyParser.json());
 
-HTTP_SERVER.listen((port), ()=>{
+HTTP_SERVER.listen((port), hostname , ()=>{
     console.log(`Server Started On http://localhost:${port}`);
 })
 
